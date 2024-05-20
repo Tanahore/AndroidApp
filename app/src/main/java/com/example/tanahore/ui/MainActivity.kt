a32002f3cde763c071854e19dd461b675806e456
riyanatsill <riyanatsil@gmail.com>
2024-05-20 18:41:50 +0700

    First Commit Camera

--- app/src/main/java/com/example/tanahore/ui/MainActivity.kt
+++ app/src/main/java/com/example/tanahore/ui/MainActivity.kt
@@ -1,21 +1,47 @@
 package com.example.tanahore.ui
 
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.content.pm.ActivityInfo
 import android.os.Bundle
+import android.os.Handler
+import android.util.Log
+import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.tanahore.R
+import com.example.tanahore.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var doubleBackToExitPressedOnce = false
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setAddButton()
+    }
+
+    private fun setAddButton(){
+        binding.fabCamera.setOnClickListener {
+            startActivity(Intent(this, SelectActivity::class.java))
         }
+    }
+
+    override fun onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed()
+            finishAffinity()
+            return
+        }
+        this.doubleBackToExitPressedOnce = true
+        Toast.makeText(this, "Press back again to exit", Toast.LENGTH_SHORT).show()
+        Handler().postDelayed({ doubleBackToExitPressedOnce = false }, 2000)
     }
 }
